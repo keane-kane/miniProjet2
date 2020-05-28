4306906a17fc4f775054a2467b9a253959d51d59
keane-kane <abdouazizk50@gmail.com>
2020-05-28 22:47:13 +0200

    new projet

---
+++ connexion.php
@@ -0,0 +1,23 @@
+﻿
+         <div class="container-fluid ">
+          <div class="div-form-con">  
+            <h2 class="login">Login Form</h2>
+            <form action="/action_page.php">
+                <div class="form-group div2 ">
+                    <label for="email"></label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <div class="form-group div2 ">
+                    <label for="pwd"></label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+                </div>
+                <div class="checkbox">
+                    <input type="checkbox" name="remember"><span>Se souvenir de moi</span>
+                </div>
+                <button type="submit" class="btn btn-default">Connexion</button>
+                <p class="inscrire"><a href="index.php?lien=inscri">S'inscrire pour jouer?</a></p>
+
+            </form>
+        </div>
+         </div>
+                
